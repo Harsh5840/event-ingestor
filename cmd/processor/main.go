a9f3f11ddc7ddb80c6543a2c3ed3c54559b3e3db
Harsh5840 <shuklaharsh5840@gmail.com>
2025-09-01 16:55:13 +0530

    processor main

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -13,4 +13,24 @@ import (
 
 func main () {
 	fmt.Println("Starting processor Service")
+
+	ctx, cancel := context.WithCancel(context.Background())    //here , we declared the context 
+	defer cancel()
+
+	//graceful shutdown
+	sigs := make(chan os.Signal , 1) 
+	signal.Notify(sigs, syscall.SIGINT , syscall.SIGTERM)  //here 
+
+	go func ()  {
+		<-sigs
+		fmt.Println("Shutting down processor service")
+		cancel()
+	}()
+
+	//starting processor logic
+	if  err := processor.Start(ctx); err != nil {
+		log.Fatalf("Processor service error : %v" , err)
+	}
+
+	fmt.Println("Processor Service Stopped")
 }
